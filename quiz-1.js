5c1422735645d4e06f6381a021830ce1450ed1e0
Kyle <kylemar99@gmail.com>
2021-10-18 15:40:30 +0000

    Add event listeners and variables for current buttons to be shown

--- quiz-1.js
+++ quiz-1.js
@@ -78,4 +78,20 @@
     const quizContainer = document.getElementById('quiz');
     const resultsContainer = document.getElementById('results');
     const submitButton = document.getElementById('submit');
-})
+
+
+    buildQuiz();
+
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    showSlide(currentSlide);
+
+    //Event listeners
+    submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener('click', showPreviousSlide);
+    nextButton.addEventListener('click', showNextSlide);
+
+})();
